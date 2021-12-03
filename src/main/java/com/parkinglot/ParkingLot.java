08300527bc58e7002c0f0f796d78353b9ede4864
CHEUNJO2 <cheunjo2@oocl.com>
2021-12-03 14:16:04 +0800

    [parking lot] [joanne] feat: implement checking capacity of lot full or not

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,14 +1,21 @@
 package com.parkinglot;
 
+import java.util.HashMap;
+
 public class ParkingLot {
   private int capacity;
+  HashMap<Ticket, Car> parkingLotMap = new HashMap<>();
 
   public ParkingLot(int capacity) {
     this.capacity = capacity;
   }
 
   public Ticket park(Car car) {
-    return new Ticket();
+    if (parkingLotMap.size() < capacity) {
+      Ticket newTicket = new Ticket();
+      parkingLotMap.put(newTicket, car);
+      return newTicket;
+    } else return null;
   }
 
   public Car fetch(Ticket ticket) {
