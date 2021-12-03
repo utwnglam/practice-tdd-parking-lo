ce8ec9ed1913855f92204857e519899aa3d70e06
CHEUNJO2 <cheunjo2@oocl.com>
2021-12-03 19:29:46 +0800

    [SuperSmartParkingBoy] [joanne] feat: implement and add case should_return_nothing_when_fetching_given_smart_parking_boy_manage_two_parking_lots_and_unrecognized_ticket

--- src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
@@ -58,20 +58,20 @@ public class SuperSmartParkingBoyTest {
     assertThat(carFetchFromSecondTicket).isEqualTo(secondParkedCar);
   }
 
-//  @Test
-//  public void should_return_nothing_when_fetching_given_smart_parking_boy_manage_two_parking_lots_and_unrecognized_ticket() {
-//    ParkingLot firstParkingLot = new ParkingLot();
-//    ParkingLot secondParkingLot = new ParkingLot();
-//    List<ParkingLot> parkingLotList = Arrays.asList(firstParkingLot, secondParkingLot);
-//
-//    SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLotList);
-//
-//    UnrecognizedTicketException unrecognizedWrongTicket = assertThrows(UnrecognizedTicketException.class, () -> {
-//      smartParkingBoy.fetch(new Ticket());
-//    });
-//    assertEquals("Unrecognized parking ticket.", unrecognizedWrongTicket.getMessage());
-//  }
-//
+  @Test
+  public void should_return_nothing_when_fetching_given_smart_parking_boy_manage_two_parking_lots_and_unrecognized_ticket() {
+    ParkingLot firstParkingLot = new ParkingLot();
+    ParkingLot secondParkingLot = new ParkingLot();
+    List<ParkingLot> parkingLotList = Arrays.asList(firstParkingLot, secondParkingLot);
+
+    SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLotList);
+
+    UnrecognizedTicketException unrecognizedWrongTicket = assertThrows(UnrecognizedTicketException.class, () -> {
+      superSmartParkingBoy.fetch(new Ticket());
+    });
+    assertEquals("Unrecognized parking ticket.", unrecognizedWrongTicket.getMessage());
+  }
+
 //  @Test
 //  public void should_return_nothing_when_fetching_given_smart_parking_boy_manage_two_parking_lots_and_used_ticket() {
 //    ParkingLot firstParkingLot = new ParkingLot();
