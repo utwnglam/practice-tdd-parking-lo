3c7e0df040c3e601b97189ac725436bf960e024c
CHEUNJO2 <cheunjo2@oocl.com>
2021-12-03 18:31:50 +0800

    [parking lot] [joanne] feat: implementshould_park_to_first_lot_when_parking_given_smart_parking_boy_manage_2lots_and_both_same_number_position

---
+++ src/main/java/com/parkinglot/SmartParkingBoy.java
@@ -0,0 +1,9 @@
+package com.parkinglot;
+
+import java.util.List;
+
+public class SmartParkingBoy extends ParkingBoy{
+  public SmartParkingBoy(List<ParkingLot> parkingLotList) {
+    super(parkingLotList);
+  }
+}
---
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -0,0 +1,91 @@
+package com.parkinglot;
+
+import com.parkinglot.exception.NoAvailablePositionException;
+import com.parkinglot.exception.UnrecognizedTicketException;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SmartParkingBoyTest {
+  @Test
+  public void should_park_to_first_lot_when_parking_given_smart_parking_boy_manage_2lots_and_both_same_number_position() {
+    ParkingLot firstParkingLot = new ParkingLot();
+    ParkingLot secondParkingLot = new ParkingLot();
+    List<ParkingLot> parkingLotList = Arrays.asList(firstParkingLot, secondParkingLot);
+
+    SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLotList);
+    Ticket ticket = smartParkingBoy.park(new Car());
+
+    assertNotNull(ticket);
+    assertEquals(9, firstParkingLot.getAvailablePosition());
+    assertEquals(10, secondParkingLot.getAvailablePosition());
+  }
+
+//  @Test
+//  public void should_return_right_when_parking_given_parking_boy_manage_two_parking_lots_and_both_have_1car() {
+//    ParkingLot firstParkingLot = new ParkingLot(1);
+//    ParkingLot secondParkingLot = new ParkingLot(1);
+//    List<ParkingLot> parkingLotList = Arrays.asList(firstParkingLot, secondParkingLot);
+//
+//    ParkingBoy parkingBoy = new ParkingBoy(parkingLotList);
+//    Car firstParkedCar = new Car();
+//    Ticket firstTicket = parkingBoy.park(firstParkedCar);
+//    Car secondParkedCar = new Car();
+//    Ticket secondTicket = parkingBoy.park(secondParkedCar);
+//
+//    Car carFetchFromFirstTicket = parkingBoy.fetch(firstTicket);
+//    Car carFetchFromSecondTicket = parkingBoy.fetch(secondTicket);
+//
+//    assertThat(carFetchFromFirstTicket).isEqualTo(firstParkedCar);
+//    assertThat(carFetchFromSecondTicket).isEqualTo(secondParkedCar);
+//  }
+//
+//  @Test
+//  public void should_return_nothing_when_fetching_given_parking_boy_manage_two_parking_lots_and_unrecognized_ticket() {
+//    ParkingLot firstParkingLot = new ParkingLot();
+//    ParkingLot secondParkingLot = new ParkingLot();
+//    List<ParkingLot> parkingLotList = Arrays.asList(firstParkingLot, secondParkingLot);
+//    ParkingBoy parkingBoy = new ParkingBoy(parkingLotList);
+//
+//    UnrecognizedTicketException unrecognizedWrongTicket = assertThrows(UnrecognizedTicketException.class, () -> {
+//      parkingBoy.fetch(new Ticket());
+//    });
+//    assertEquals("Unrecognized parking ticket.", unrecognizedWrongTicket.getMessage());
+//  }
+//
+//  @Test
+//  public void should_return_nothing_when_fetching_given_parking_boy_manage_two_parking_lots_and_used_ticket() {
+//    ParkingLot firstParkingLot = new ParkingLot();
+//    ParkingLot secondParkingLot = new ParkingLot();
+//    List<ParkingLot> parkingLotList = Arrays.asList(firstParkingLot, secondParkingLot);
+//    ParkingBoy parkingBoy = new ParkingBoy(parkingLotList);
+//
+//    Ticket ticket = parkingBoy.park(new Car());
+//    parkingBoy.fetch(ticket);
+//    UnrecognizedTicketException unrecognizedUsedTicket = assertThrows(UnrecognizedTicketException.class, () -> {
+//      parkingBoy.fetch(ticket);
+//    });
+//    assertEquals("Unrecognized parking ticket.", unrecognizedUsedTicket.getMessage());
+//  }
+//
+//  @Test
+//  public void should_return_nothing_when_parking_given_parking_boy_manage_two_parking_lots_and_both_unavailable() {
+//    ParkingLot firstParkingLot = new ParkingLot(1);
+//    ParkingLot secondParkingLot = new ParkingLot(1);
+//    List<ParkingLot> parkingLotList = Arrays.asList(firstParkingLot, secondParkingLot);
+//    ParkingBoy parkingBoy = new ParkingBoy(parkingLotList);
+//    firstParkingLot.park(new Car());
+//    secondParkingLot.park(new Car());
+//
+//    NoAvailablePositionException noAvailablePositionException = assertThrows(NoAvailablePositionException.class, () -> {
+//      parkingBoy.park(new Car());
+//    });
+//    assertEquals("No available position.", noAvailablePositionException.getMessage());
+//  }
+}
