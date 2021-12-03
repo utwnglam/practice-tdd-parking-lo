68338d3a943d6d7104904535896de02cf42378ff
CHEUNJO2 <cheunjo2@oocl.com>
2021-12-03 14:12:27 +0800

    [parking lot] [joanne] test: add case should_return_null_when_parking_lot_is_full_given_a_car

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,6 +1,12 @@
 package com.parkinglot;
 
 public class ParkingLot {
+  private int capacity;
+
+  public ParkingLot(int capacity) {
+    this.capacity = capacity;
+  }
+
   public Ticket park(Car car) {
     return new Ticket();
   }
--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -3,11 +3,12 @@ package com.parkinglot;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ParkingLotTest {
   @Test
   public void should_return_ticket_when_parking_given_a_car() {
-    ParkingLot parkingLot = new ParkingLot();
+    ParkingLot parkingLot = new ParkingLot(10);
     Car car = new Car();
 
     Ticket ticket = parkingLot.park(car);
@@ -16,10 +17,21 @@ public class ParkingLotTest {
 
   @Test
   public void should_return_a_car_when_fetching_given_a_ticket() {
-    ParkingLot parkingLot = new ParkingLot();
+    ParkingLot parkingLot = new ParkingLot(10);
     Ticket ticket = new Ticket();
 
     Car car = parkingLot.fetch(ticket);
     assertNotNull(car);
+  }
+
+  @Test
+  public void should_return_null_when_parking_lot_is_full_given_a_car() {
+    ParkingLot parkingLot = new ParkingLot(1);
+    parkingLot.park(new Car());
+
+    Car car = new Car();
+
+    Ticket ticket = parkingLot.park(car);
+    assertNull(ticket);
   }
 }
